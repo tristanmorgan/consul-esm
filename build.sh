50580804d571fe3fda02482601294736fc9a3275
Tristan Morgan <tristan.morgan@hashicorp.com>
2024-12-17 08:53:13 +1100

    update github.com/prometheus/client_golang

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 TS_VAR=$(date +%s)
-docker build --platform=linux/amd64 -t registry.service.home.consul/consul-esm:$TS_VAR-amd64 -f Dockerfile-goreleaser .
+docker build --platform=linux/amd64 -t registry.service.home.consul/consul-esm:$TS_VAR-amd64 .
 docker push registry.service.home.consul/consul-esm:$TS_VAR-amd64
-docker build --platform=linux/arm64/v8 -t registry.service.home.consul/consul-esm:$TS_VAR-arm64 -f Dockerfile-goreleaser .
+docker build --platform=linux/arm64/v8 -t registry.service.home.consul/consul-esm:$TS_VAR-arm64 .
 docker push registry.service.home.consul/consul-esm:$TS_VAR-arm64
 
 docker manifest create registry.service.home.consul/consul-esm:$TS_VAR --amend registry.service.home.consul/consul-esm:$TS_VAR-arm64 --amend registry.service.home.consul/consul-esm:$TS_VAR-amd64
